3efafc3f1752c56a5df56395af36f60df380788b
aaroncorona <amc1140@berkeley.edu>
2023-01-14 22:09:15 -0800

    Temporarily remove the button binding for testing purposes

--- app/src/main/java/com/textscheduler/NewTextFragment.java
+++ app/src/main/java/com/textscheduler/NewTextFragment.java
@@ -1,13 +1,9 @@
 package com.textscheduler;
 
-import android.app.AlarmManager;
-import android.content.Context;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -36,21 +32,6 @@ public class NewTextFragment extends Fragment {
             public void onClick(View view) {
                 NavHostFragment.findNavController(NewTextFragment.this)
                         .navigate(R.id.action_NewTextFragment_to_HomeFragment);
-            }
-        });
-
-        binding.buttonSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Get text parameters from the text boxes
-                String phone = String.valueOf(binding.textPhoneInput.getText());
-                String date = String.valueOf(binding.textDateInput.getText());
-                String time = String.valueOf(binding.textTimeInput.getText());
-                String message = String.valueOf(binding.textMessageInput.getText());
-                // Text message settings
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(phone, null, message, null, null);
-                // TODO Schedule settings
             }
         });
     }
