6859cc5fcc58e07b22f100c63a12f83db55aa735
aaroncorona <amc1140@berkeley.edu>
2023-01-22 09:28:28 -0800

    Split the home screen tests into smaller tests

--- app/src/androidTest/java/com/textscheduler/MainActivityTest.java
+++ app/src/androidTest/java/com/textscheduler/MainActivityTest.java
@@ -19,21 +19,23 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class) // Only needed when mixing JUnit 3 and 4 tests
 public class MainActivityTest {
-
-    private static final String stringToBeTyped = "Espresso";
-
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void buttonClick_navigatesToPage() {
-        // First, check that the buttons exist
+    public void newTextButtonClick_navigatesToNewTextPage() {
+        // Check that the buttons exist
         assertNotNull(onView(withId(R.id.button_new)));
-        assertNotNull(onView(withId(R.id.button_existing)));
 
         // When the New Text button is clicked, the New Text screen is displayed
         onView(withId(R.id.button_new)).perform(click());
         onView(withId(R.id.button_submit)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void existingTextsButtonClick_navigatesToExistingTextsPage() {
+        // Check that the buttons exist
+        assertNotNull(onView(withId(R.id.button_existing)));
 
         // When the Existing Texts button is clicked, the Existing Texts screen is displayed
         onView(withId(R.id.button_existing)).perform(click());
